0f2ef63405fb8e9fcb6bc8ed4b4eb9eb38dde42d
rosemqa <ers6wobmo09m@mail.ru>
2024-05-21 01:30:38 +0300

    added getenv

--- config/data.py
+++ config/data.py
@@ -1,10 +1,10 @@
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
 
 class Data:
 
-    LOGIN = "Admin" # os.getenv("LOGIN")
-    PASSWORD = "admin123" # os.getenv("PASSWORD")
+    LOGIN = os.getenv("LOGIN")    # "Admin"
+    PASSWORD = os.getenv("PASSWORD")     # "admin123"
